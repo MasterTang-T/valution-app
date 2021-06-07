31d2badcd8f16792d64c765d34c136f49046da56
Tang_tom <1286545749@qq.com>
2021-06-07 16:18:04 +0800

    基础计算逻辑

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,17 +1,17 @@
 <template>
 <div class="main_app">
     <div class="header">
-        <el-button type="primary" class="addCompany" size="small"> 新增公司 </el-button>
+        <el-button type="primary" class="addCompany" size="small" @click="changeDialogVisible"> 新增公司 </el-button>
     </div>
     <div class="content">
         <el-table :data="tableData" style="width: 100%" stripe size='small'>
             <el-table-column label="公司名称" prop="companyName"></el-table-column>
             <el-table-column label="自由现金流" prop="freeCashFlow" width="100"></el-table-column>
-            <el-table-column label="3年后利润" prop="profit"  width="100"></el-table-column>
+            <el-table-column label="3年后利润" prop="profit" width="100"></el-table-column>
             <el-table-column label="25倍PE" prop="pe25"></el-table-column>
             <el-table-column label="30倍PE" prop="pe30"></el-table-column>
             <el-table-column label="高点值" prop="pe50"></el-table-column>
-            <el-table-column label="无风险收益率" prop="noRisk"  width="120"></el-table-column>
+            <el-table-column label="无风险收益率" prop="noRisk" width="120"></el-table-column>
             <el-table-column label="无风险收益率x2" prop="noRiskDouble" width="120"></el-table-column>
             <el-table-column label="操作" width="200">
                 <template slot-scope="scope">
@@ -21,6 +21,23 @@
             </el-table-column>
         </el-table>
     </div>
+    <el-dialog title="提示" :visible="dialogVisible" >
+        <el-form ref="form" :rules="rules" :model="form" label-width="110px">
+            <el-form-item label="公司名称" prop='companyName'>
+                <el-input v-model="form.companyName" placeholder="公司名称"></el-input>
+            </el-form-item>
+            <el-form-item label="自由现金流" prop='freeCashFlow'>
+                <el-input v-model="form.freeCashFlow" placeholder="自由现金流,可理解为净利润"></el-input>
+            </el-form-item>
+            <el-form-item label="无风险收益率" prop='noRisk'>
+                <el-input v-model="form.noRisk" placeholder="例如:0.04"></el-input>
+            </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="changeDialogVisible">取 消</el-button>
+            <el-button type="primary" @click="submitDialogForm">确 定</el-button>
+        </span>
+    </el-dialog>
 </div>
 </template>
 
@@ -29,27 +46,31 @@ export default {
     name: 'app',
     data() {
         return {
-            tableData: [{
-                    companyName: '海康威视',
-                    freeCashFlow: 137,
-                    profit: 456,
-                    pe25: 1234,
-                    pe25: 2345,
-                    pe50: 3456,
-                    noRisk: '4%',
-                    noRiskDouble: '8%'
-                },
-                {
-                    companyName: '海康威视2',
-                    freeCashFlow: 137,
-                    profit: 456,
-                    pe25: 1234,
-                    pe25: 2345,
-                    pe50: 3456,
-                    noRisk: '4%',
-                    noRiskDouble: '8%'
-                }
-            ]
+            dialogVisible: false,
+            form: {
+                companyName: '',
+                freeCashFlow: '',
+                noRisk: ''
+            },
+            rules: {
+                companyName: [{
+                        required: true,
+                        message: '公司名称',
+                        trigger: 'blur'
+                    },
+                ],
+                freeCashFlow: [{
+                    required: true,
+                    message: '自由现金流',
+                    trigger: 'blur'
+                }],
+                noRisk: [{
+                    required: true,
+                    message: '无风险收益率',
+                    trigger: 'blur'
+                }],
+            },
+            tableData: []
         }
     },
     methods: {
@@ -58,7 +79,39 @@ export default {
         },
         handleDelete(index, row) {
             console.log(index, row);
+        },
+        changeDialogVisible() {
+            const _this = this;
+            _this.dialogVisible = !_this.dialogVisible;
+        },
+        submitDialogForm() {
+            const _this = this;
+            _this.calculateTable();
+            _this.dialogVisible = !_this.dialogVisible;
+            _this.form.companyName = '';
+            _this.form.freeCashFlow = '';
+            _this.form.noRisk = '';
+        },
+        calculateTable(){
+            const _this = this;
+            let companyName,freeCashFlow,profit,pe25,pe30,pe50,noRisk,noRiskDouble;
+            noRiskDouble = _this.form.noRisk * 2;
+            profit = _this.form.freeCashFlow * Number(Math.pow(1+noRiskDouble,3).toFixed(2));
+            pe25 = profit * 25;
+            pe30 = profit * 30;
+            pe50 = profit * 30 * 1.5;
+            _this.tableData.push({
+                companyName:_this.form.companyName,
+                freeCashFlow:_this.form.freeCashFlow,
+                profit,
+                pe25,
+                pe30,
+                pe50,
+                noRisk:_this.form.noRisk,
+                noRiskDouble
+            })
         }
+        
     },
 }
 </script>
@@ -77,6 +130,6 @@ export default {
 
 .header {
     text-align: right;
-    margin-bottom:10px;
+    margin-bottom: 10px;
 }
 </style>
--- src/popup/index.js
+++ src/popup/index.js
@@ -6,12 +6,20 @@ import {
   Card,
   Button,
   Table,
-  TableColumn
+  TableColumn,
+  Input,
+  Dialog,
+  Form,
+  FormItem
 } from 'element-ui';
 Vue.use(Card);
 Vue.use(Button);
 Vue.use(Table);
 Vue.use(TableColumn);
+Vue.use(Input);
+Vue.use(Dialog);
+Vue.use(Form);
+Vue.use(FormItem);
 new Vue({
   el: "#app",
   render: createElement => {
